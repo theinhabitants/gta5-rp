b6a099213bc41b2314729169f24aafb9c4455bb1
Maksym Burko <themakc135@gmail.com>
2019-10-16 00:37:00 +0300

    login template

--- client_packages/login/js/main.js
+++ client_packages/login/js/main.js
@@ -8,6 +8,15 @@ function getPlayerPassword() {
     return document.getElementsByClassName("password-input_login")[0].value;   
 }
 
-function login() {
-    alert(getPlayerEmail());
+function login(email, password) {
+    let creds; //From SQL query
+    if((email !== null && email === creds[0])
+    && (password !== null && password === creds[1])) {
+        mp.gui.chat.push("You are successfully login to the server!");
+    } else {
+        mp.events.add("playerQuit", (player) => {
+            mp.gui.chat.push("You were!");
+        });
+        throw "Wrong credentials!";
+    }
 }
