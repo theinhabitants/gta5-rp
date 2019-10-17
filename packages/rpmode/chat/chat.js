b8e45a04df88b038c7e2b6dd3b1633d44ae8c540
Denis Burko <ealightpawn@gmail.com>
2019-10-17 23:22:16 +0300

    chat ready version 0.1

--- packages/rpmode/chat/chat.js
+++ packages/rpmode/chat/chat.js
@@ -1,6 +1,6 @@
 mp.events.add("playerChat", (player, message) => {
     let date = new Date();
-    const time = "[" + ("0" + (date.getHours() + 1)).slice(-2) + ":" + ("0" + (date.getMinutes() + 1)).slice(-2)  + ":" + ("0" + (date.getSeconds() + 1)).slice(-2)  + "]";
+    const time = "[" + ("0" + (date.getHours())).slice(-2) + ":" + ("0" + (date.getMinutes())).slice(-2)  + ":" + ("0" + (date.getSeconds())).slice(-2)  + "]";
     const id = `[${player.id}]`
     let str = time + " - " + "!{white}" + player.name + id + ": " + message;
     mp.players.broadcastInRange(player.position, 25, str);
