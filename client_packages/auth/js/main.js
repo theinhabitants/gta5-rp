062606c6fb027db61667ff83160a2d7a7f5a2882
TwoySenpai <themaximkaa@gmail.com>
2019-10-24 00:23:56 +0300

    Minor refactoring

--- client_packages/auth/js/main.js
+++ client_packages/auth/js/main.js
@@ -1,10 +1,24 @@
-function login() {
-    $('.alert').hide();
+$("#form").on("submit", function () {
+    $(".alert").hide();
+    const email = $("#email").val();
+    const password = $("#password").val();
 
-    const email = document.getElementById("email");
-    const password = document.getElementById("password");
-    mp.trigger("login", email.value, password.value);
-}
+    mp.trigger("login", email, password);
+    return false;
+});
+
+let isPass = true;
+$('#hide').on('click', function () {
+    if (isPass) {
+        $('#password')[0].type = 'text';
+        $('#hide')[0].src = 'img/show.png';
+        isPass = false;
+    } else {
+        $('#password')[0].type = 'password';
+        $('#hide')[0].src = 'img/hide.png';
+        isPass = true;
+    }
+});
 
 function registration() {
     const email = document.getElementById("email");
