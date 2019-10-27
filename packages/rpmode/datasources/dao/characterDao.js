f7be92348b69012a324c5dfd303108573a91f205
TwoySenpai <themaximkaa@gmail.com>
2019-10-27 03:05:45 +0300

    fix bug

--- packages/rpmode/datasources/dao/characterDao.js
+++ packages/rpmode/datasources/dao/characterDao.js
@@ -1,19 +1,25 @@
 const connectionPool = require('../mysql');
 
-const getByUserId = async function (userId) {
-    const result = await connectionPool.query('SELECT * FROM character WHERE user_id = ?', [userId]);
-    if (result[0].length !== 1) {
-        throw new Error('Character with user_id ' + userId + 'not found');
-    }
-    let characterFromDb = result[0][0];
-    return {
-        id: characterFromDb.id,
-        name: characterFromDb.name,
-        surname: characterFromDb.surname,
-        level: characterFromDb.level,
-        scin: characterFromDb.scin,
-        userId: userId
-    };
+const getByUserID = function (id) {
+    return new Promise(function (resolve, reject) {
+        connectionPool.query("SELECT * FROM `character` WHERE user_id = ?", [id], function (err, res) {
+            if (err) {
+                reject(err);
+            }
+
+            let characterFromDB = res[0];
+            if (characterFromDB === undefined) {
+                resolve(null);
+            } else {
+                resolve({
+                    id: characterFromDB.id,
+                    name: characterFromDB.name,
+                    surname: characterFromDB.surname,
+                    skin: characterFromDB.skin
+                });
+            }
+        });
+    });
 };
 
-module.exports.getByUserId = getByUserId;
+module.exports.getByUserID = getByUserID;
