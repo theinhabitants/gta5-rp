68c0094d7b76a9c8cc2902676b5380b40af51659
TwoySenpai <themaximkaa@gmail.com>
2019-10-28 01:34:33 +0200

    added log err if DB is down

--- packages/rpmode/datasources/mysql.js
+++ packages/rpmode/datasources/mysql.js
@@ -1,11 +1,19 @@
 let mysql = require('mysql');
 
-let pool = mysql.createPool({
-  host     : 'localhost',
-  port     : '3306',
-  user     : 'root',
-  password : 'root',
-  database : 'gtarp'
+const config = {
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'gtarp'
+};
+
+let pool = mysql.createPool(config);
+
+// Added to avoid situations if DB is down
+pool.getConnection(function (err) {
+    console.log(err);
+    if (err) throw err; // not connected!
 });
 
 module.exports = pool;
